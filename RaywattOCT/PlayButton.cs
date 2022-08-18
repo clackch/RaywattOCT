a679424292c37079bd262729a35d910a476fbf8c
Jinsu Kim <jeansu.kim@raywatt.com>
2022-08-18 14:58:34 +0900

    Apply RepeatButton to Prev, Next Button
    누르고 있으면 연속 실행 가능하도록 적용

--- RaywattOCT/PlayButton.cs
+++ RaywattOCT/PlayButton.cs
@@ -15,4 +15,13 @@ namespace RaywattOCT
         public string Icon { get => (string)GetValue(IconProperty); set => SetValue(IconProperty, value); }
         public string IconOv { get => (string)GetValue(IconOvProperty); set => SetValue(IconOvProperty, value); }
     }
+
+    class PlayButtonRepeat : System.Windows.Controls.Primitives.RepeatButton
+    {
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(string), typeof(PlayButtonRepeat));
+        public static readonly DependencyProperty IconOvProperty = DependencyProperty.Register("IconOv", typeof(string), typeof(PlayButtonRepeat));
+
+        public string Icon { get => (string)GetValue(IconProperty); set => SetValue(IconProperty, value); }
+        public string IconOv { get => (string)GetValue(IconOvProperty); set => SetValue(IconOvProperty, value); }
+    }
 }
