639fbe1c09a223c5cf2a5a7f9a820e9364ed34ee
whenilinde <ts@raywatt.com>
2022-10-06 10:47:10 +0900

    Code 정책
    Code 값 정책 적용

--- RaywattApp/RaywattApp/ViewModels/PatientNewViewModel.cs
+++ RaywattApp/RaywattApp/ViewModels/PatientNewViewModel.cs
@@ -21,6 +21,9 @@ namespace RaywattApp.ViewModels
         [ObservableProperty]
         private Patient _patient;
 
+        [ObservableProperty]
+        private string _genderCode;
+
         private ICommand _cancelCommand;
         public ICommand CancelCommand
         {
@@ -119,14 +122,16 @@ namespace RaywattApp.ViewModels
             commandParameters["lastname"] = Patient.Lastname;
             commandParameters["firstname"] = Patient.Firstname;
             commandParameters["birthdate"] = Patient.Birthdate;
-            if (Patient.Gender != null)
+            if (GenderCode != null)
             {
-                commandParameters["gender"] = Patient.Gender;
+                commandParameters["gender"] = GenderCode;
+                Patient.Gender = GenderCode == "M" ? "Male" : "Female";
             }
             else
             {
                 commandParameters["gender"] = "";
-            }
+            }              
+
             commandText =
                 $"INSERT INTO rv_schema.patient(id, lastname, firstname, birthdate, gender, create_date, update_date) " +
                 $"VALUES (@id, @lastname, @firstname, @birthdate, @gender, now(), now())";
@@ -136,9 +141,6 @@ namespace RaywattApp.ViewModels
 
             if(nRows == 1)
             {
-                if(Patient.Gender != null)
-                    Patient.Gender = Patient.Gender == "M" ? "Male" : "Female";
-
                 WeakReferenceMessenger.Default.Send(new NavigationMessage("Views/RecordingPage.xaml") { Parameter = Patient });
             }           
         }
