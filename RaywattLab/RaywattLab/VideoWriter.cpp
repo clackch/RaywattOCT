62c84b14d14ea7701039c528df59f9506f907e89
Jinsu Kim <jeansu.kim@raywatt.com>
2022-10-24 15:33:47 +0900

    Save Video bug fixed
    Queue 구현 방식 (배열 -> 벡터) 변경 (영상 저장 시, 처음 1초 정도 black image 저장 되는 오류 수정)

--- RaywattLab/RaywattLab/VideoWriter.cpp
+++ RaywattLab/RaywattLab/VideoWriter.cpp
@@ -7,11 +7,8 @@ CVideoWriter::CVideoWriter() {
 
 	m_nVideoWidth = 0;
 	m_nVideoHeight = 0;
-
-	m_pFrameQueue = NULL;
-	m_nQueueCount = 10;
-	m_nQueueStart = 0;
-	m_nQueueEnd = 0;
+	
+	m_nSavedFrame = 0;
 }
 CVideoWriter::~CVideoWriter() {
 	StopRecording();
@@ -21,7 +18,7 @@ CVideoWriter::~CVideoWriter() {
 int CVideoWriter::StartRecording(CString strFilePath, int nWidth, int nHeight) {
 	if (m_pThread == NULL) {
 		char strVideoFilePath[MAX_PATH];
-		int nFrameRate = 30;
+		int nFrameRate = 10;
 		wcstombs(strVideoFilePath, strFilePath.GetBuffer(), MAX_PATH);
 
 		bool result = m_videoWriter.open(strVideoFilePath, cv::CAP_ANY,	cv::VideoWriter::fourcc('A','V','I','1'), nFrameRate, cv::Size(nWidth, nHeight), true);
@@ -30,7 +27,6 @@ int CVideoWriter::StartRecording(CString strFilePath, int nWidth, int nHeight) {
 			m_nVideoWidth = nWidth;
 			m_nVideoHeight = nHeight;
 
-			initialize();
 			CUtility::StartThread(threadWriteVideo, m_pThread, this);
 		}
 		else {
@@ -47,39 +43,27 @@ void CVideoWriter::StopRecording() {
 void CVideoWriter::PushToBuffer(cv::Mat image) {
 	if (m_pThread == nullptr || !m_pThread->isRun) return;
 
+	cv::Mat imgCopy;
 	if (image.rows != m_nVideoHeight || image.cols != m_nVideoWidth) {
-		cv::resize(image, m_pFrameQueue[m_nQueueEnd], m_pFrameQueue[m_nQueueEnd].size());
+		cv::resize(image, imgCopy, cv::Size(m_nVideoWidth, m_nVideoHeight));
 	}
 	else {
-		image.copyTo(m_pFrameQueue[m_nQueueEnd]);
+		imgCopy = image.clone();
 	}
-	m_nQueueEnd++;
-	m_nQueueEnd = (m_nQueueEnd >= m_nQueueCount) ? 0 : m_nQueueEnd;
+	m_vFrameQueue.push_back(imgCopy);
 }
 
-void CVideoWriter::initialize() {
-	finalize();
-	
-	m_pFrameQueue = new cv::Mat[m_nQueueCount];
-	for (int i = 0; i < m_nQueueCount; i++) {
-		m_pFrameQueue[i].create(m_nVideoHeight, m_nVideoWidth, CV_8UC3);
-	}	
-}
 void CVideoWriter::finalize() {
-	if (m_pFrameQueue != NULL) {
-		for (int i = 0; i < m_nQueueCount; i++) {
-			m_pFrameQueue[i].release();
-		}
-		delete[] m_pFrameQueue;
-		m_pFrameQueue = NULL;
+	for (int i = 0; i < m_vFrameQueue.size(); i++) {
+		m_vFrameQueue[i].release();
 	}
+	m_vFrameQueue.clear();
+	m_nSavedFrame = 0; 
 }
 bool CVideoWriter::popFromBuffer(int& nPopIndex) {
-	if (m_nQueueStart != m_nQueueEnd) {
-		m_nQueueStart++;
-		m_nQueueStart = (m_nQueueStart >= m_nQueueCount) ? 0 : m_nQueueStart;
-
-		nPopIndex = m_nQueueStart;
+	if (m_nSavedFrame != m_vFrameQueue.size()) {
+		nPopIndex = m_nSavedFrame;
+		m_nSavedFrame++;
 
 		return true;
 	}
@@ -88,16 +72,15 @@ bool CVideoWriter::popFromBuffer(int& nPopIndex) {
 }
 UINT CVideoWriter::threadWriteVideo(LPVOID param) {
 	CVideoWriter* pWriter = (CVideoWriter*)param;
-	cv::Mat* pMatQueue = pWriter->m_pFrameQueue;
 	int nPopIndex = 0;
 
 	while (pWriter->m_pThread->isRun) {
 		if (pWriter->popFromBuffer(nPopIndex)) {
-			pWriter->m_videoWriter.write(pMatQueue[nPopIndex]);
+			pWriter->m_videoWriter.write(pWriter->m_vFrameQueue[nPopIndex]);
 		}
 	}
 	while (pWriter->popFromBuffer(nPopIndex)) {
-		pWriter->m_videoWriter.write(pMatQueue[nPopIndex]);
+		pWriter->m_videoWriter.write(pWriter->m_vFrameQueue[nPopIndex]);
 	}
 
 	return NOERROR;
