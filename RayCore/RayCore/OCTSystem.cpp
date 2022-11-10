5ed484b77f7adbf0b68901ddcb875262390391b0
Jinsu Kim <jeansu.kim@raywatt.com>
2022-11-10 13:07:53 +0900

    bug fixed

--- RayCore/RayCore/OCTSystem.cpp
+++ RayCore/RayCore/OCTSystem.cpp
@@ -83,7 +83,6 @@ RayError COCTSystem::Start() {
 
 	m_pCutView = new CCutViewManager();
 	m_pVolume = new CVolumeGenerator();
-	m_pVolume->Initialize(config.nCircleSize, config.nCircleSize, config.nVolumeSize, config.nVolumeSize);
 
 	m_pAcqDevice = new CATSDevice();
 	m_pAcqDevice->SetImaging(m_pImagingRealtime);
