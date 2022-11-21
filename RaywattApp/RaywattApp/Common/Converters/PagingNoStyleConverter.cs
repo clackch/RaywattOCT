fed6bb5d5936d69983e89b0325314514cfe53f75
whenilinde <ts@raywatt.com>
2022-11-21 17:23:21 +0900

    버튼 style 기능 추가
    버튼 클릭에 따라 색상 변경되도록 기능 변경

--- RaywattApp/RaywattApp/Common/Converters/PagingNoStyleConverter.cs
+++ RaywattApp/RaywattApp/Common/Converters/PagingNoStyleConverter.cs
@@ -1,24 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
 
 namespace RaywattApp.Common.Converters
 {
-    public class PagingNoCheckedConverter : IValueConverter
+    public class PagingNoStyleConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             int nPagingNoIdx = (int)value;
             int nParameter = int.Parse(parameter.ToString());
+            var ButtonImportant = App.Current.Resources["Important"];
+            var ButtonBasic = App.Current.Resources["Basic"];
 
             if (nPagingNoIdx % 5 == nParameter)
-                return "Red";
+                return ButtonImportant;
             else
-                return "Black";
+                return ButtonBasic;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
