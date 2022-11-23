c664dd4a00811d0687a00f91af01bb191f6c7bbc
whenilinde <ts@raywatt.com>
2022-11-23 18:04:45 +0900

    Recording 화면 버튼 기능 수정
    Recording 화면 버튼 기능 수정

--- RaywattApp/RaywattApp/ViewModels/ReviewPresetViewModel.cs
+++ RaywattApp/RaywattApp/ViewModels/ReviewPresetViewModel.cs
@@ -44,9 +44,6 @@ namespace RaywattApp.ViewModels
         private bool _isPreset;
 
         [ObservableProperty]
-        private Visibility _notPresetMode;
-
-        [ObservableProperty]
         private Visibility _modifyMode;
 
         [ObservableProperty]
@@ -156,7 +153,6 @@ namespace RaywattApp.ViewModels
                     PatientCasePresetList = _sqlManager.SelectPatientCasePresetList();
 
                     _isPreset = true;
-                    NotPresetMode = Visibility.Collapsed;
                     ModifyMode = Visibility.Collapsed;
                     SelectionMode = Visibility.Visible;
                 }
@@ -173,7 +169,6 @@ namespace RaywattApp.ViewModels
                     PatientCasePresetList.Add(patientCasePreset);
 
                     _isPreset = false;
-                    NotPresetMode = Visibility.Visible;
                     ModifyMode = Visibility.Visible;
                     SelectionMode = Visibility.Collapsed;
                 }
@@ -233,11 +228,22 @@ namespace RaywattApp.ViewModels
         {
             _log.Debug("Back");
 
-            Dictionary<string, object> parameter = new Dictionary<string, object>();
-            parameter["patient"] = Patient;
-            parameter["patientCase"] = PatientCase;
-            parameter["prevStatus"] = PrevStatus;
-            WeakReferenceMessenger.Default.Send(new NavigationMessage("Views/ReviewPage.xaml") { Parameter = parameter });
+            if (_isPreset)
+            {
+                Dictionary<string, object> parameter = new Dictionary<string, object>();
+                parameter["patient"] = Patient;
+                parameter["patientCase"] = PatientCase;
+                parameter["prevStatus"] = PrevStatus;
+                WeakReferenceMessenger.Default.Send(new NavigationMessage("Views/RecordingPage.xaml") { Parameter = parameter });
+            }
+            else
+            {
+                Dictionary<string, object> parameter = new Dictionary<string, object>();
+                parameter["patient"] = Patient;
+                parameter["patientCase"] = PatientCase;
+                parameter["prevStatus"] = PrevStatus;
+                WeakReferenceMessenger.Default.Send(new NavigationMessage("Views/ReviewPage.xaml") { Parameter = parameter });
+            }
         }
 
         private void New()
