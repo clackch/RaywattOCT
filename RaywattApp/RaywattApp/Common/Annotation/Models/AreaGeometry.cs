5c4493b628a2dbacbc7df7913fc33b10f7348641
Jinsu Kim <jeansu.kim@raywatt.com>
2022-12-21 11:01:50 +0900

    Add area diameter values
    무게 중심 구하는 방식 변경 (cv contour moments 사용)
    무게 중심 기준 360º 방향으로 diameter 연산 (영상 subtraction)
    min, max, mean diameter 구하고, 화면에 min, max line 표시

--- RaywattApp/RaywattApp/Common/Annotation/Models/AreaGeometry.cs
+++ RaywattApp/RaywattApp/Common/Annotation/Models/AreaGeometry.cs
@@ -4,6 +4,11 @@ using System.Windows.Shapes;
 
 namespace RaywattApp.Common.Annotation.Models
 {
+    public struct DiameterInfo
+    {
+        public Point point1, point2;
+        public double diameter;
+    }
     public class AreaGeometry
     {
         private List<Point> points;
@@ -17,5 +22,26 @@ namespace RaywattApp.Common.Annotation.Models
 
         private double area;
         public double Area { get { return area; } set { area = value; } }
+
+        private List<OpenCvSharp.Point> pointsAll;
+        public List<OpenCvSharp.Point> PointsAll { get { return pointsAll; } set { pointsAll = value; } }
+
+        private Point centerOfMass;
+        public Point CenterOfMass { get { return centerOfMass; } set { centerOfMass = value; } }
+
+        private DiameterInfo minDiameter;
+        public DiameterInfo MinDiameter { get { return minDiameter; } set { minDiameter = value; } }
+
+        private DiameterInfo maxDiameter;
+        public DiameterInfo MaxDiameter { get { return maxDiameter; } set { maxDiameter = value; } }
+
+        private double meanDiameter;
+        public double MeanDiameter { get { return meanDiameter; } set { meanDiameter = value; } }
+
+        private bool validDiameter;
+        public bool ValidDiameter { get { return validDiameter; } set { validDiameter = value; } }
+
+        private Path path;
+        public Path Path { get { return path; } set { path = value; } }
     }
 }
