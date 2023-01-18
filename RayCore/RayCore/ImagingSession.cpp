460a57e31f009bf4cf42504987aff95a460f1065
Jinsu Kim <jeansu.kim@raywatt.com>
2023-01-18 12:40:07 +0900

    add ImagingSession class
    동시에 여러 case 를 review 하는 경우를 위해 관리용 class 생성

---
+++ RayCore/RayCore/ImagingSession.cpp
@@ -0,0 +1,104 @@
+#include "ImagingSession.h"
+#include "Utility.h"
+#include "MessageService.h"
+#include "OCTImaging.h"
+#include "SimulateDevice.h"
+#include "DataReader.h"
+#include "Configuration.h"
+
+CImagingSession::CImagingSession(CMessageService* pMsg, int nSession, bool deleteData) :
+	m_pMsg(pMsg),
+	m_nSession(nSession),
+	m_deleteData(deleteData)
+{
+	m_pImaging = nullptr;
+	m_pSimDevice = nullptr;
+	m_pDataManager = nullptr;
+}
+CImagingSession::~CImagingSession() {
+	Stop();
+	if (m_pImaging != nullptr) delete m_pImaging;
+	if (m_pSimDevice != nullptr) delete m_pSimDevice;
+	if (m_deleteData && m_pDataManager != nullptr) delete m_pDataManager;
+}
+
+CImagingSession* CImagingSession::CreateSession(CMessageService* pMsg, int nSession, IDataManager* pWriter) {
+	if (pMsg == nullptr || pWriter == nullptr) return nullptr;
+
+	return createSession(pMsg, nSession, pWriter);
+}
+
+CImagingSession* CImagingSession::CreateSession(CMessageService* pMsg, int nSession, const char* strFilePath) {
+	CDataReader* pReader = new CDataReader();
+	int nNumOfSamples = pReader->Initialize(CUtility::StringToWstring(strFilePath));
+
+	if (pMsg == nullptr || nNumOfSamples <= 0) {
+		delete pReader;
+		return nullptr;
+	}
+
+	return createSession(pMsg, nSession, pReader);
+}
+
+void CImagingSession::EnableWorkItem(RayWorkItem item, bool enable) {
+	switch (item) {
+	case RayWorkItem::UpdateCutView:
+		break;
+	case RayWorkItem::GenerateVolume:
+		break;
+	default:
+		break;
+	}
+}
+int CImagingSession::Start() {
+	if (m_pSimDevice == nullptr || m_pImaging == nullptr) return -1;
+
+	m_pImaging->Start();
+	return m_pSimDevice->StartAcquisition();
+}
+
+int CImagingSession::Stop() {
+	if(m_pImaging != nullptr) m_pImaging->Stop();
+	if(m_pSimDevice != nullptr) m_pSimDevice->StopAcquisition();
+
+	return NOERROR;
+}
+
+bool CImagingSession::IsPaused() {
+	if (m_pSimDevice == nullptr) return true;
+	return m_pSimDevice->IsPaused();
+}
+void CImagingSession::SetPause(bool pause) {
+	if (m_pSimDevice != nullptr) m_pSimDevice->SetPause(pause);
+}
+void CImagingSession::PrevFrame() {
+	if (m_pSimDevice != nullptr) m_pSimDevice->PrevFrame();
+}
+void CImagingSession::NextFrame() {
+	if (m_pSimDevice != nullptr) m_pSimDevice->NextFrame();
+}
+void CImagingSession::MoveToFrame(int nFrame) {
+	if (m_pSimDevice != nullptr) m_pSimDevice->SetFrame(nFrame);
+}
+
+CImagingSession* CImagingSession::createSession(CMessageService* pMsg, int nSession, IDataManager* pData) {
+	CImagingSession* pSession = new CImagingSession(pMsg, nSession);
+
+	pSession->m_pDataManager = pData;
+	pSession->m_pImaging = CreateColorImaging(pMsg);
+	pSession->m_pSimDevice = new CSimulateDevice(pData);
+	pSession->m_pSimDevice->InitDevice();
+	pSession->m_pSimDevice->SetImaging(pSession->m_pImaging);
+
+	return pSession;
+}
+COCTImaging* CImagingSession::CreateColorImaging(CMessageService* msg) {
+	COCTImaging* pImaging = new COCTImaging(msg);
+	CConfiguration& config = CConfiguration::GetInstance();
+
+	pImaging->Initialize(_T("CALIBRATION.DAT"));
+	pImaging->SetColor(true);
+	pImaging->SetBrightnessContrast(config.imaging.brightness, config.imaging.contrast);
+
+	return pImaging;
+}
---
+++ RayCore/RayCore/ImagingSession.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "define.h"
+
+class CMessageService;
+class COCTImaging;
+class CSimulateDevice;
+class IDataManager;
+class CImagingSession
+{
+private:
+	CMessageService* m_pMsg;
+	int m_nSession;
+
+	COCTImaging* m_pImaging;
+	CSimulateDevice* m_pSimDevice;
+	IDataManager* m_pDataManager;
+
+	bool m_deleteData;
+
+private:
+	CImagingSession(CMessageService* pMsg, int nSession, bool deleteData = true);
+public:
+	virtual ~CImagingSession();
+
+	static CImagingSession* CreateSession(CMessageService* pMsg, int nSession, IDataManager *pWriter);
+	static CImagingSession* CreateSession(CMessageService* pMsg, int nSession, const char* strFilePath);
+	static COCTImaging* CreateColorImaging(CMessageService* msg);
+
+	IDataManager* GetDataManager() { return m_pDataManager; }
+	void EnableWorkItem(RayWorkItem item, bool enable);
+	int Start();
+	int Stop();
+	bool IsPaused();
+	void SetPause(bool pause);
+	void PrevFrame();
+	void NextFrame();
+	void MoveToFrame(int nFrame);
+
+private:
+	static CImagingSession* createSession(CMessageService* pMsg, int nSession, IDataManager* pData);
+	
+};
+
