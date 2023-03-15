e29b1d92638859f81cfabfbcba29dd19674164f6
Jinsu Kim <jeansu.kim@raywatt.com>
2023-03-15 13:14:19 +0900

    update IconButton component class
    상위 인터페이스 (SvgComponentBase) 정의
    Component 별 하위 클래스 생성

---
+++ RaywattApp/RaywattApp/Views/Component/IndicatorImage.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace RaywattApp.Views.Component
+{
+    public class IndicatorImage : Image, SvgComponentBase
+    {
+        private const string resPath = "/res/indicator/";
+
+        public static DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(string), typeof(IndicatorImage), new PropertyMetadata(null, SvgComponentBase.OnIconPropertyChanged));
+        public string Icon { get => (string)GetValue(IconProperty); set => SetValue(IconProperty, value); }
+
+        public void InitializeIconPath(string iconName)
+        {
+            IconDefault = SvgComponentBase.GetPath(resPath, iconName);
+            IconOver = SvgComponentBase.GetPath(resPath, iconName, "_hover");
+        }
+
+        public static readonly DependencyProperty IconDefaultProperty = DependencyProperty.Register(nameof(IconDefault), typeof(string), typeof(IndicatorImage));
+        public static readonly DependencyProperty IconOverProperty = DependencyProperty.Register(nameof(IconOver), typeof(string), typeof(IndicatorImage));
+
+        public string IconDefault { get => (string)GetValue(IconDefaultProperty); set => SetValue(IconDefaultProperty, value); }
+        public string IconOver { get => (string)GetValue(IconOverProperty); set => SetValue(IconOverProperty, value); }
+    }
+}
