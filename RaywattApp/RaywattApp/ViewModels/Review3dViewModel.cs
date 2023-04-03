be293a28c449a7840d163261c38075ea754c166f
whenilinde <ts@raywatt.com>
2023-04-03 14:27:28 +0900

    3D Design

--- RaywattApp/RaywattApp/ViewModels/Review3dViewModel.cs
+++ RaywattApp/RaywattApp/ViewModels/Review3dViewModel.cs
@@ -1,13 +1,20 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using log4net;
 using RaywattApp.Common.Bases;
+using RaywattApp.Common.Dialog;
 using RaywattApp.Common.Messages;
 using RaywattApp.Models;
+using RaywattApp.Services;
 using RaywattOCT;
 using System;
 using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
 using System.Windows.Navigation;
+using System.Windows.Threading;
+using static RaywattOCT.RayCoreWrapper;
 
 namespace RaywattApp.ViewModels
 {
@@ -15,11 +22,108 @@ namespace RaywattApp.ViewModels
     {
         private static readonly ILog _log = LogManager.GetLogger(typeof(Review3dViewModel));
 
-        public Review3dViewModel()
+        // size from view
+        private Point crossSectionCenterBig = new Point();
+        private Point longitudeCoordinate = new Point();
+
+        [ObservableProperty]
+        private bool _isCutViewOn;
+
+        [ObservableProperty]
+        private bool _isIndicatorOn;
+
+        [ObservableProperty]
+        private bool _isPtoD;
+
+        [ObservableProperty]
+        private bool _isSideBranchView;
+
+        [ObservableProperty]
+        private bool _isTissueOn;
+
+        [ObservableProperty]
+        private bool _isLumenOn;
+
+        [ObservableProperty]
+        private bool _isStentOn;
+
+        [ObservableProperty]
+        private bool _isGuidewireOneOn;
+
+        [ObservableProperty]
+        private bool _isGuidewireTwoOn;
+
+        private double degree;
+        public double Degree
+        {
+            get { return degree; }
+            set { degree = value; OnPropertyChanged(nameof(Degree)); RaySetProperty(Property.LongitudeDegree, degree); }
+        }
+
+        [ObservableProperty]
+        private Indicator _indicatorCrossSection;
+
+        [ObservableProperty]
+        private Indicator _indicatorLongitude;
+
+        [ObservableProperty]
+        private double _pointLongitudeX;
+
+        [ObservableProperty]
+        private bool _isPaused;
+
+        private DispatcherTimer timerUpdateImage = new DispatcherTimer();
+
+        private ICommand _cmdRotateIndicator;
+        public ICommand CmdRotateIndicator
+        {
+            get { return this._cmdRotateIndicator ?? (this._cmdRotateIndicator = new RelayCommand<object>(RotateIndicator)); }
+        }
+
+        private ICommand _cmdMoveIndicator;
+        public ICommand CmdMoveIndicator
+        {
+            get { return this._cmdMoveIndicator ?? (this._cmdMoveIndicator = new RelayCommand<object>(MoveIndicator)); }
+        }
+
+        private ICommand _cmdViewSizeChanged;
+        public ICommand CmdViewSizeChanged
+        {
+            get { return this._cmdViewSizeChanged ?? (this._cmdViewSizeChanged = new RelayCommand<object>(ViewSizeChanged)); }
+        }
+
+        private double _lModeIndicatorX;
+        public double LModeIndicatorX
+        {
+            get { return _lModeIndicatorX; }
+            set { _lModeIndicatorX = value; OnPropertyChanged(nameof(LModeIndicatorX)); setCurrentFrame(value); }
+        }
+
+        public Review3dViewModel(SqlManager sqlManager, IDialogService dialogService) : base(sqlManager, dialogService)
         {
             _log.Debug("Review3dViewModel");
 
             Constants.CurrentPage = Constants.Review3dPage;
+
+            IndicatorCrossSection = new Indicator();
+            IndicatorCrossSection.IsVisible = Visibility.Visible;
+
+            IndicatorLongitude = new Indicator();
+            IndicatorLongitude.X = Constants.LongitudeIndicatorWidth / 2;
+            IndicatorLongitude.IsVisible = Visibility.Collapsed;
+
+            IsCutViewOn = true;
+            IsIndicatorOn = true;
+            IsPtoD = true;
+            IsSideBranchView = false;
+
+            IsTissueOn = true;
+            IsLumenOn = false;
+            IsStentOn = false;
+            IsGuidewireOneOn = true;
+            IsGuidewireTwoOn = false;
+
+            updatePlayPauseState();
         }
 
         public override void OnNavigated(object sender, object navigatedEventArgs)
@@ -37,13 +141,137 @@ namespace RaywattApp.ViewModels
                 PrevStatus = (PrevStatus)data["prevStatus"];
                 ReviewStatus = (ReviewStatus)data["reviewStatus"];
                 ReviewStatus.CurrentPage = Constants.Review3dPage;
+
+                Degree = PatientCase.IndicatorDegree;
+
+                RaySetProperty(Property.LongitudeBackgroundColor, Constants.CardBackgroundColor);
+                SetCrossSectionBackground(0, (ReviewStatus.IsAngioOn) ? Constants.CardBackgroundColor : Constants.BackgroundColor);
             }
+
+            timerUpdateImage.Interval = TimeSpan.FromMilliseconds(Constants.UpdateImageInterval);
+            timerUpdateImage.Tick += new EventHandler(timerFuncUpdateImage);
+            timerUpdateImage.Start();
         }
 
         public override void OnNavigating(object sender, object navigationEventArgs)
         {
             base.OnNavigating(sender, navigationEventArgs);
             _log.Debug("OnNavigating");
+
+            if (timerUpdateImage.IsEnabled)
+                timerUpdateImage.Stop();
+        }
+
+        private void timerFuncUpdateImage(object sender, EventArgs e)
+        {
+            if (DrawCrossSectionImage())
+            {
+                if (!IndicatorLongitude.IsCaptured) updateNavigator(crossSectionFrameInfo[0].curFrame, crossSectionFrameInfo[0].totalFrame);
+
+                FrameNumber = crossSectionFrameInfo[0].curFrame;
+            }
+            if (DrawLongitudeImage())
+            {
+                // when generating longitude image is completed
+                if (longitudeFrameInfo.curFrame == longitudeFrameInfo.totalFrame)
+                {
+                    IndicatorLongitude.IsVisible = Visibility.Visible;
+                }
+            }
+        }
+
+        private void updatePlayPauseState()
+        {
+            double pauseState = RayGetProperty(Property.IsPaused);
+
+            IsPaused = (pauseState == 0);
+        }
+
+        private void updateNavigator(int curFrame, int totalFrame)
+        {
+            double curPosition = (double)curFrame / (totalFrame - 1);
+            curPosition *= Constants.LongitudeWidth3d;
+            IndicatorLongitude.X = curPosition - Constants.LongitudeIndicatorWidth / 2;
+            IndicatorLongitude.CenterX = curPosition;
+        }
+
+        private void RotateIndicator(object param)
+        {
+            Indicator indicator = (Indicator)param;
+
+            if (indicator.IsCaptured)
+            {
+                Point crossSectionCenter;                
+                crossSectionCenter = crossSectionCenterBig;
+
+                indicator.SetDirection(crossSectionCenter, Degree);
+                if (!indicator.IsValid) return;
+
+                Point headerSidePoint = new Point(indicator.X, indicator.Y);
+                if (indicator.OppositeCaptured)
+                {
+                    double xOffset = indicator.X - crossSectionCenter.X;
+                    double yOffset = indicator.Y - crossSectionCenter.Y;
+
+                    headerSidePoint.X = crossSectionCenter.X - xOffset;
+                    headerSidePoint.Y = crossSectionCenter.Y - yOffset;
+                }
+
+                double pointX = crossSectionCenter.X - headerSidePoint.X;
+                double pointY = crossSectionCenter.Y - headerSidePoint.Y;
+                Degree = (int)(Math.Atan2(pointY, pointX) * 180 / Math.PI);
+            }
+        }
+
+        private void MoveIndicator(object param)
+        {
+            Indicator indicator = (Indicator)param;
+
+            if (indicator.IsCaptured)
+            {
+                double x = PointLongitudeX - longitudeCoordinate.X;
+
+                if (x >= 0 && x < Constants.LongitudeWidth3d)
+                {
+                    indicator.X = x - Constants.LongitudeIndicatorWidth / 2;
+                    setCurrentFrame(x);
+                }
+            }
+        }
+
+        private void ViewSizeChanged(object param)
+        {
+            if (param != null)
+            {
+                FrameworkElement frameworkElement = (FrameworkElement)param;
+
+                Point pointWindow = Application.Current.MainWindow.PointToScreen(new Point(0, 0));
+                Point point = frameworkElement.PointToScreen(new Point(0, 0));
+                point.X -= pointWindow.X;
+                point.Y -= pointWindow.Y;
+
+                if (frameworkElement.Name.Equals("crossSectionImage"))
+                {
+                    crossSectionCenterBig.X = point.X + (frameworkElement.ActualWidth / 2);
+                    crossSectionCenterBig.Y = point.Y + (frameworkElement.ActualHeight / 2);
+                }
+                else if (frameworkElement.Name.Equals("lMode"))
+                {
+                    longitudeCoordinate = point;
+                }
+            }
+        }
+
+        private void setCurrentFrame(double navigatorPosition)
+        {
+            double curPosition = navigatorPosition / Constants.LongitudeWidth3d;
+
+            if (longitudeFrameInfo != null)
+            {
+                curPosition *= (longitudeFrameInfo.totalFrame - 1);
+                curPosition = Math.Round(curPosition);
+                RayMoveToFrame((int)curPosition);
+            }
         }
     }
 }
