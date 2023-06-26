1c374252a8da01bbf954463074930875cfca3a6f
whenilinde <ts@raywatt.com>
2023-06-26 15:40:18 +0900

    Longitude Indicator 조작 수정
    - Play 중일 때,
      - 마우스로 indicator 잡을 경우, Pause로 전환
      - Prev/Next 클릭 시, Pause로 전환

--- RaywattApp/RaywattApp/ViewModels/ReviewViewModel.cs
+++ RaywattApp/RaywattApp/ViewModels/ReviewViewModel.cs
@@ -357,10 +357,16 @@ namespace RaywattApp.ViewModels
 
             if (action.ToLower().Equals("prev"))
             {
+                if (!DeviceStatus.IsPaused)
+                    Playback();
+
                 PrevFrame(RaySession.Review);
             }
             else if (action.ToLower().Equals("next"))
             {
+                if (!DeviceStatus.IsPaused)
+                    Playback();
+
                 NextFrame(RaySession.Review);
             }
             else if (action.ToLower().Equals("play"))
@@ -431,6 +437,9 @@ namespace RaywattApp.ViewModels
             {
                 if (indicator.IsLongitudeClicked)
                 {
+                    if (!DeviceStatus.IsPaused)
+                        Playback();
+
                     indicator.IsLongitudeClicked = false;
                     return;
                 }
