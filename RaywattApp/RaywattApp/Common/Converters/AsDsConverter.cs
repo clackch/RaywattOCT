66df5a6ec0c0343e1f761fc4e11c40b81f791682
whenilinde <ts@raywatt.com>
2023-07-31 15:22:49 +0900

    명칭 변경

--- RaywattApp/RaywattApp/Common/Converters/AsDsConverter.cs
+++ RaywattApp/RaywattApp/Common/Converters/AsDsConverter.cs
@@ -4,7 +4,7 @@ using System.Windows.Data;
 
 namespace RaywattApp.Common.Converters
 {
-    public class PercentValueConverter : IMultiValueConverter
+    public class AsDsConverter : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
