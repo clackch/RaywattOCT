805284ac8009f24637c355cd44a8c1de31b2baa8
Jinsu Kim <jeansu.kim@raywatt.com>
2023-08-08 20:27:52 +0900

    add OpenChildWindow
    Modeless dialog 를 생성하기 위한 함수 및 클래스 추가

--- RaywattApp/RaywattApp/Common/Dialog/DialogService.cs
+++ RaywattApp/RaywattApp/Common/Dialog/DialogService.cs
@@ -47,5 +47,44 @@ namespace RaywattApp.Common.Dialog
 
             return dialogDataContext.DialogResult;
         }
+        public IDialogWindow OpenChildWindow(object dialog, IModelessPatient parent, Dictionary<string, object> parameter, double width, double height, double left, double top)
+        {
+            var dialogFE = dialog as FrameworkElement;
+            var dialogDataContext = dialogFE.DataContext as ModelessViewModelBase;
+            Window mainWindow = Application.Current.MainWindow;
+
+            dialogDataContext.DialogWidth = width;
+            dialogDataContext.DialogHeight = height;
+
+            IDialogWindow window = new DialogWindow();
+            window.Content = dialog;
+            window.DataContext = dialogDataContext;
+
+            if (double.NaN.Equals(left))
+            {
+                window.Left = mainWindow.Left + (mainWindow.Width - width) / 2;
+            }
+            else
+            {
+                window.Left = left;
+            }
+
+            if (double.NaN.Equals(top))
+            {
+                window.Top = mainWindow.Top + (mainWindow.Height - height) / 2;
+            }
+            else
+            {
+                window.Top = top;
+            }
+
+            if (parameter != null)
+                dialogDataContext.SetParameter(parent, parameter);
+
+            window.Owner = Application.Current.Windows.OfType<Window>().SingleOrDefault(x => x.IsActive);
+            window.Show();
+
+            return window;
+        }
     }
 }
