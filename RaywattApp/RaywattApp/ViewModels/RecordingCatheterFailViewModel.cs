ebd5563da39bb6132492808bf5dd7b86b74e5d3c
whenilinde <ts@raywatt.com>
2023-08-17 17:27:00 +0900

    로고 변경 및 기타 수정
    - 로고 변경
    - Catheter 연결/해제 기능 추가
      - 이미지 변경

--- RaywattApp/RaywattApp/ViewModels/RecordingCatheterFailViewModel.cs
+++ RaywattApp/RaywattApp/ViewModels/RecordingCatheterFailViewModel.cs
@@ -17,40 +17,23 @@ namespace RaywattApp.ViewModels
     {
         private static readonly ILog _log = LogManager.GetLogger(typeof(RecordingCatheterFailViewModel));
 
+        private DispatcherTimer timer = new DispatcherTimer();
+
         [ObservableProperty]
         private Patient _patient;
 
         [ObservableProperty]
         private PrevStatus _prevStatus;
 
-        [ObservableProperty]
-        private bool _isStep1;
-
-        private ICommand _cancelCommand;
-        public ICommand CancelCommand
-        {
-            get { return this._cancelCommand ?? (this._cancelCommand = new RelayCommand(Cancel)); }
-        }
-
-        private ICommand _nextStepCommand;
-        public ICommand NextStepCommand
-        {
-            get { return this._nextStepCommand ?? (this._nextStepCommand = new RelayCommand(NextStep)); }
-        }
-
-        private ICommand _nextCommand;
-        public ICommand NextCommand
-        {
-            get { return this._nextCommand ?? (this._nextCommand = new RelayCommand(Next)); }
-        }
-
         public RecordingCatheterFailViewModel()
         {
             _log.Debug("RecordingCatheterFailViewModel");
 
             Constants.CurrentPage = Constants.RecordingCatheterFailPage;
-            
-            IsStep1 = true;
+
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+            timer.Tick += new EventHandler(CheckCatheterStatus);
+            timer.Start();
         }
 
         public override void OnNavigated(object sender, object navigatedEventArgs)
@@ -64,63 +47,18 @@ namespace RaywattApp.ViewModels
                 Dictionary<string, Object> data = (Dictionary<string, Object>)extraData;
                 this.Patient = (Patient)data["patient"];
                 this.PrevStatus = (PrevStatus)data["prevStatus"];
+
+                if (DeviceStatus.CatheterStatus == Constants.CatheterStatusUnloaded)
+                    Next();
             }
         }
 
         public override void OnNavigating(object sender, object navigationEventArgs)
         {
             _log.Debug("OnNavigating");
-        }
 
-        private void Cancel()
-        {
-            _log.Debug("Cancel");
-
-            if(Patient == null || PrevStatus == null)
-            {
-                WeakReferenceMessenger.Default.Send(new NavigationMessage(Constants.PatientListPage));
-            }
-            else
-            {
-                Dictionary<string, object> parameter = new Dictionary<string, object>();
-                parameter["patient"] = this.Patient;
-                parameter["prevStatus"] = this.PrevStatus;
-                WeakReferenceMessenger.Default.Send(new NavigationMessage(Constants.PatientDetailPage) { Parameter = parameter });
-            }
-        }
-
-        private DispatcherTimer timer = new DispatcherTimer();//Test
-
-        private void NextStep()
-        {
-            _log.Debug("NextStep");
-
-            IsStep1 = false;
-
-            //Test
-            timer.Interval = TimeSpan.FromMilliseconds(2000);
-            timer.Tick += new EventHandler(StepChange);
-            timer.Start();
-        }
-
-        //Test
-        private void StepChange(object sender, EventArgs e)
-        {
-            _log.Debug("StepChange : " + DeviceStatus.CatheterStatus);
-
-            if (DeviceStatus.CatheterStatus == Constants.CatheterStatusFailed)
-            {
-                DeviceStatus.CatheterStatus = Constants.CatheterStatusUnlocked;
-            }
-            else if(DeviceStatus.CatheterStatus == Constants.CatheterStatusUnlocked)
-            {
-                DeviceStatus.CatheterStatus = Constants.CatheterStatusUnloaded;
-            }
-            else if (DeviceStatus.CatheterStatus == Constants.CatheterStatusUnloaded)
-            {
-                DeviceStatus.CatheterStatus = Constants.CatheterStatusDisconnected;
+            if (timer.IsEnabled)
                 timer.Stop();
-            }
         }
 
         private void Next()
@@ -138,6 +76,12 @@ namespace RaywattApp.ViewModels
                 parameter["prevStatus"] = this.PrevStatus;
                 WeakReferenceMessenger.Default.Send(new NavigationMessage(Constants.RecordingSetupPage) { Parameter = parameter });
             }
+        }
+
+        private void CheckCatheterStatus(object sender, EventArgs e)
+        {
+            if (DeviceStatus.CatheterStatus == Constants.CatheterStatusUnloaded)
+                Next();
         }
     }
 }
