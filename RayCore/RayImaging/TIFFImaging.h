880bccd3f9ad0f4708a06dd899a37a65de3f45fa
changhwanOh <101632343+clackch@users.noreply.github.com>
2023-10-11 14:33:59 +0900

    극좌표에서 직교좌표 변환(TIF)

--- RayCore/RayImaging/TIFFImaging.h
+++ RayCore/RayImaging/TIFFImaging.h
@@ -5,7 +5,11 @@ class CTIFFImaging : public COCTImaging
 {
 private:
 	cv::Mat imageConvert;	// role of OCTImaging::imageResult
+	cv::Mat dematXMap;
+	cv::Mat dematYMap;
 	std::chrono::system_clock::time_point m_start, m_end;
+	int count = 0;
+	int countPost = 0;
 
 public:
 	CTIFFImaging(Setting, CMessageService*);
@@ -16,5 +20,6 @@ public:
 	virtual void PostProcess(cv::Mat image);
 	virtual cv::Mat GetProcessedImage() { return imageConvert; }
 	virtual void CircularizeImage(cv::Mat& src, cv::Mat& dst);
+	virtual void initCircularizeMap(int diameter, int srcHeight, int srcWidth, int dstHeight, int dstWidth, double scale);
 };
 
