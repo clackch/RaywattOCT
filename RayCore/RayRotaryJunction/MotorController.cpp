0f5b01bc7eb487952962ccdaccd571b659cfb7e6
Jinsu Kim <jeansu.kim@raywatt.com>
2023-10-31 14:01:49 +0900

    Update MotorController.cpp
    BLDC velocity=0 으로 설정해도 회전하는 오류 수정

--- RayCore/RayRotaryJunction/MotorController.cpp
+++ RayCore/RayRotaryJunction/MotorController.cpp
@@ -79,14 +79,7 @@ bool CMotorController::PerformRun(int &nVelocity) {
 	int packetLength = 0;
 	bool result = false;
 
-	if (nVelocity < 0) {
-		// negative
-		nVelocity = (nVelocity < -50000) ? -50000 : (nVelocity > -100) ? -100 : nVelocity;
-	}
-	else {
-		// positive
-		nVelocity = (nVelocity > 50000) ? 50000 : (nVelocity < 100) ? 100 : nVelocity;
-	}
+	nVelocity = (nVelocity > 50000) ? 50000 : (nVelocity < 0) ? 0 : nVelocity;
 
 	getMotorPacket(MOTOR_INDEX_TARGETVELOCITY, nVelocity, 4, packet, packetLength);
 	result = writeMotor(packet, packetLength);
