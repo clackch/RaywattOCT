d9302a717405cf18c6409fe6ab4e694c9f5ae71f
whenilinde <ts@raywatt.com>
2024-01-24 18:22:20 +0900

    Update Constants.cs

--- RaywattApp/RaywattApp/Common/Bases/Constants.cs
+++ RaywattApp/RaywattApp/Common/Bases/Constants.cs
@@ -279,7 +279,7 @@ namespace RaywattApp.Common.Bases
         public const double SectionValueWidth = 24;
         public const double SectionValueCenterWidth = 0.25;
         public const double LumenProfileExtraHeight = 6;
-        public const double PreLesionLengthInitValue = 10;
+        public const double PreLesionLengthInitValue = 15;
         public const double PostLesionLengthInitValue = 4;
         public const int LumenProfileStentMinCount = 5;
 
