0fa8ec43d9ccb67355d22ffe064d3ebcaec36d05
whenilinde <ts@raywatt.com>
2024-03-20 16:44:21 +0900

    코드값 수정

---
+++ RaywattApp/RaywattApp/Views/PhysicianEditPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using RaywattApp.ViewModels;
+using System.Windows.Controls;
+
+namespace RaywattApp.Views
+{
+    /// <summary>
+    /// PhysicianEditPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class PhysicianEditPage : Page
+    {
+        public PhysicianEditPage()
+        {
+            InitializeComponent();
+            DataContext = App.Current.Services.GetService(typeof(PhysicianEditViewModel));
+        }
+    }
+}
