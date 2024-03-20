1d2a78b0f2441792dd66d10d97600c0552c62915
whenilinde <ts@raywatt.com>
2024-03-20 17:25:40 +0900

    PatientCase에 설정 관련 필드 추가

--- RaywattApp/RaywattApp/Models/PatientCase.cs
+++ RaywattApp/RaywattApp/Models/PatientCase.cs
@@ -44,9 +44,6 @@ namespace RaywattApp.Models
             }
         }
 
-        [ObservableProperty]
-        private string accessionName;
-
         private string _comment;
         public string Comment
         {
@@ -62,10 +59,13 @@ namespace RaywattApp.Models
         }
 
         [ObservableProperty]
+        private string procedure;
+
+        [ObservableProperty]
         private string vessel;
 
         [ObservableProperty]
-        private string procedure;
+        private string location;
 
         [ObservableProperty]
         private int numOfFrames;
@@ -104,19 +104,14 @@ namespace RaywattApp.Models
         [ObservableProperty]
         private double _indicatorDegree;
 
-        private string? _presetName;
-        public string PresetName
-        {
-            get { return _presetName; }
-            set
-            {
-                if (value.Length <= Constants.MaxPatientCasePresetName)
-                {
-                    _presetName = value;
-                    OnPropertyChanged(nameof(PresetName));
-                }
-            }
-        }
+        [ObservableProperty]
+        private string? _flushMedia;
+
+        [ObservableProperty]
+        private string? _pullbackTrigger;
+
+        [ObservableProperty]
+        private string? _colormap;
 
         [ObservableProperty]
         private int _calciumThreshold;
