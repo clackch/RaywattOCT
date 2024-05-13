b846a9877254c7b22f221dc9ffd16717090f3100
changhwanOh <101632343+clackch@users.noreply.github.com>
2024-05-13 21:00:30 +0900

    0516 Demo대비 3D쪽 Brightness 수정
    App쪽 Rendermode 호출 시점 변경 (Stent Full 버그 있었음.)

--- RaywattApp/RaywattApp/ViewModels/Review3dViewModel.cs
+++ RaywattApp/RaywattApp/ViewModels/Review3dViewModel.cs
@@ -210,6 +210,7 @@ namespace RaywattApp.ViewModels
             }
 
             IsRendering = false;
+            ODSOCT_SetRenderMode(true);
 
             threadInitialize = new Thread(() => threadFuncInitialize());
             threadInitialize.Start();
@@ -339,7 +340,6 @@ namespace RaywattApp.ViewModels
             //ODSOCT_InputData(Ray3DObject.GuideWire, buffer, diameter, diameter, depth, 1, 1, zVal);
 
             ODSOCT_ProcessingDatas();
-            ODSOCT_SetRenderMode(true);
 
             Marshal.FreeHGlobal(buffer);
             timerShowData.Interval = TimeSpan.FromMilliseconds(MinWaitingDelay);
