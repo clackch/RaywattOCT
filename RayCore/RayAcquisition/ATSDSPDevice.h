4fc5c2c8670840f74ab1b091d8fb5507b7294d32
Jinsu Kim <jeansu.kim@raywatt.com>
2024-08-06 11:30:42 +0900

    add ATSDSPDevice class
    FPGA 사용을 위한 class 추가

---
+++ RayCore/RayAcquisition/ATSDSPDevice.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "ATSDevice.h"
+#include "AlazarDSP.h"
+
+class CATSDSPDevice : public CATSDevice
+{
+private:
+	dsp_module_handle m_fftHandle;
+	U32 m_bytesPerBuffer;
+	unsigned short* m_pBackgroundFringes;
+
+public:
+	CATSDSPDevice(Setting);
+	virtual ~CATSDSPDevice();
+
+protected:
+	virtual int stop();
+	virtual char* acquire(int& nCurFrame, int& nTotalFrame);
+	virtual BOOL configureFPGA(HANDLE boardHandle);
+	virtual BOOL calculateMemorySize(HANDLE boardHandle, U16 channelMask, U32 recordsPerBuffer, U32& samplesPerRecord, U32& bytesPerBuffer);
+	int getFFTLength(unsigned int nAScan);
+};
+
