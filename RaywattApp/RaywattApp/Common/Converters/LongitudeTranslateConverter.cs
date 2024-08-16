35c2f78965690a8e119c85907aa9218e3381b527
whenilinde <ts@raywatt.com>
2024-08-16 11:42:42 +0900

    Longitude FoV 적용

---
+++ RaywattApp/RaywattApp/Common/Converters/LongitudeTranslateConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RaywattApp.Common.Converters
+{
+    internal class LongitudeTranslateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return Binding.DoNothing;
+
+            double scale = 10.0 / (double)value;
+            double height = (double)parameter;
+
+            return -1 * (scale - 1) * height / 2;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
