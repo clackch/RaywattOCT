f3c6b193da194e34db0766f65ccbf99ccea5aa6c
whenilinde <ts@raywatt.com>
2024-08-19 15:50:27 +0900

    Angio Recording bug fixed
    - 기존에는 Angio 영상이 나오지 않아도(Chp 파일 연결 안되는 경우), Angio 장비만 연결되면 촬영에 Angio 영상이 있다고 체크가 되어 있었음

--- RaywattApp/RaywattApp/ViewModels/RecordingConfirmViewModel.cs
+++ RaywattApp/RaywattApp/ViewModels/RecordingConfirmViewModel.cs
@@ -129,7 +129,7 @@ namespace RaywattApp.ViewModels
             PatientCase.Id = Patient.Id + "_" + DateTime.Now.ToString("yyyyMMddHHmmss");
             PatientCase.PatientId = Patient.Id;
             PatientCase.NumOfFrames = numOfFrames;
-            PatientCase.AngioYn = DeviceStatus.IsAngioConnected;
+            PatientCase.AngioYn = DeviceStatus.IsAngioInitialized;
             PatientCase.IndicatorDegree = 90;            
 
             Dictionary<string, Object> sqlParameters = new Dictionary<string, Object>();
