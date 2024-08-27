a4c06a37f9e27ac159ee366119784b55c05099c9
s1lv3rrud <86763857+s1lv3rrud@users.noreply.github.com>
2024-08-27 13:42:47 +0900

    fix: LiveView 진입 시 CathRoom 선택 창 분기

--- RaywattApp/RaywattApp/ViewModels/RecordingLiveViewViewModel.cs
+++ RaywattApp/RaywattApp/ViewModels/RecordingLiveViewViewModel.cs
@@ -189,7 +189,7 @@ namespace RaywattApp.ViewModels
                 RaySetProperty(Property.PullbackDistance, Double.Parse(PbLength));
                 RaySetProperty(Property.PullbackSpeed, Double.Parse(PbSpeed));
 
-                if (!DeviceStatus.IsAngioInitialized && DeviceStatus.IsAngioConnected)
+                if (!DeviceStatus.IsAngioInitialized && DeviceStatus.IsAngioConnected && DeviceStatus.SelectedCathRoom.Id != -1)
                 {
                     _angioManager.SelectCathRoom();
                 }
