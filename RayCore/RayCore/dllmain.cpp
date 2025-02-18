ac38211e5b02b1f8a73cdbb7a95b1d33a6e26c1b
changhwanOh <101632343+clackch@users.noreply.github.com>
2025-02-18 18:34:45 +0900

    Update dllmain.cpp

--- RayCore/RayCore/dllmain.cpp
+++ RayCore/RayCore/dllmain.cpp
@@ -266,8 +266,8 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     {
     case DLL_PROCESS_ATTACH:
     case DLL_THREAD_ATTACH:
-        AllocConsole();
-        freopen("CONOUT$", "w", stdout);
+        /*AllocConsole();
+        freopen("CONOUT$", "w", stdout);*/
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
         break;
