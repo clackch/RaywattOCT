9272e901f58696d75ee447197e885de2576322d8
changhwanOh <101632343+clackch@users.noreply.github.com>
2025-05-12 16:00:31 +0900

    Python file name update

--- RaywattApp/RaywattApp/Common/Util/CommonUtil.cs
+++ RaywattApp/RaywattApp/Common/Util/CommonUtil.cs
@@ -881,7 +881,7 @@ namespace RaywattApp.Common.Util
                     _log.Debug($"sys.path: {sys.path}");
 
                     // Python 모듈 가져오기
-                    dynamic script = Py.Import("SaveTIFFAsGray");
+                    dynamic script = Py.Import("ImageProcess");
 
                     // Mat 리스트를 Python으로 전달
                     int width, height;
