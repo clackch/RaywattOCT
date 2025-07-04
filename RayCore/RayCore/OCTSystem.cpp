1f74f86aa886e78bfccc2b5cbc103807c354f631
changhwanOh <101632343+clackch@users.noreply.github.com>
2025-07-04 09:40:27 +0900

    DAQ vs Delayline Interference fix
    ConnectDevices에서 Laser, DAQ 초기화를 가장 앞으로 이동

--- RayCore/RayCore/OCTSystem.cpp
+++ RayCore/RayCore/OCTSystem.cpp
@@ -207,8 +207,6 @@ RayError COCTSystem::ConnectDevices() {
 	int result = NOERROR;
 
 	if (m_curState == RayScannerState::Initial) {
-		result |= connectRotaryJunction();
-		PLOGI.printf("connect Rotary Junction and Laser Module - %s", ((result == NOERROR) ? "Succeed" : "Failed"));
 
 		// Connect to COM Interface first time
 		CLaserController* pLaser = CLaserController::GetInstance();
@@ -218,6 +216,9 @@ RayError COCTSystem::ConnectDevices() {
 		PLOGI.printf("connect DAQ - %s", ((result == NOERROR) ? "Succeed" : "Failed"));
 
 		pLaser->LaserOnOff(false);
+
+		result |= connectRotaryJunction();
+		PLOGI.printf("connect Rotary Junction and Laser Module - %s", ((result == NOERROR) ? "Succeed" : "Failed"));
 
 		if (m_isTestMode) {
 			if (m_pRJController->IsConnected() == false)
@@ -2204,9 +2205,9 @@ int COCTSystem::disconnectRotaryJunction() {
 
 	if (m_pLaserModule->IsConnected()) {
 		CConfiguration& config = CConfiguration::GetInstance();
-		/*m_pLaserModule->Set(eStepMotorIndex::DelayLine, CM_SM_SPEED_MAX * (config.laserModule.delayLineSMSteps == 1 ? 1 : 2) * 2);
+		m_pLaserModule->Set(eStepMotorIndex::DelayLine, CM_SM_SPEED_MAX * (config.laserModule.delayLineSMSteps == 1 ? 1 : 2));
 		m_pLaserModule->Current(eStepMotorIndex::DelayLine, DELAY_LINE_UPPER_END_POSITION * config.laserModule.delayLineSMSteps);
-		m_pLaserModule->Move(eStepMotorIndex::DelayLine, 0, false, static_cast<char>(0x03));*/
+		m_pLaserModule->Move(eStepMotorIndex::DelayLine, 0, false, static_cast<char>(0x03));
 		m_pLaserModule->Move(eStepMotorIndex::Polarization, 0);
 		m_pLaserModule->SetVLD(0);
 		m_pLaserModule->SetVOA(0);
