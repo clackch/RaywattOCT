b85fea2acf1d6edbcd8119f82584cd87ae092682
raywattTS <110812182+raywattTS@users.noreply.github.com>
2025-07-09 16:20:36 +0900

    에러 예외 처리
    - RJState가 Error인 상태에서, Preset 화면에서 Next 버튼으로 Catheter Fail 화면으로 넘어가면서, PatientCase 객체의 Procedure, Vessel, Location에 Null 값이 들어가는데, 다시 Preset 화면에 들어오면, Procedure 값 등이 Null이라서 에러 발생
    => Catheter Fail 화면으로 들어갈 때도 Procedure 등에도 값을 넣어서 넘기도록 수정 및 Null 예외 처리

--- RaywattApp/RaywattApp/ViewModels/RecordingPresetViewModel.cs
+++ RaywattApp/RaywattApp/ViewModels/RecordingPresetViewModel.cs
@@ -117,16 +117,26 @@ namespace RaywattApp.ViewModels
                 this.Patient = (Patient)data["patient"];
                 this.PrevStatus = (PrevStatus)data["prevStatus"];
 
-                if (data.ContainsKey("patientCase"))
+                if (data.TryGetValue("patientCase", out var patientCaseObj) && patientCaseObj is PatientCase caseData)
                 {
-                    PatientCase = (PatientCase)data["patientCase"];
-                    if (CodeDefinition.Codes["PROC"].ContainsKey(PatientCase.Procedure))
-                        CurrentProcedure = new KeyValuePair<string, string>(PatientCase.Procedure, CodeDefinition.Codes["PROC"][PatientCase.Procedure]);
-                    if (CodeDefinition.Codes["VESS"].ContainsKey(PatientCase.Vessel))
-                        CurrentVessel = new KeyValuePair<string, string>(PatientCase.Vessel, CodeDefinition.Codes["VESS"][PatientCase.Vessel]);
-                    if (CodeDefinition.Codes["LOCT"].ContainsKey(PatientCase.Location))
-                        CurrentLocation = new KeyValuePair<string, string>(PatientCase.Location, CodeDefinition.Codes["LOCT"][PatientCase.Location]);
+                    PatientCase = caseData;
+
+                    if (CodeDefinition.Codes.TryGetValue("PROC", out var procDict) && procDict.TryGetValue(PatientCase.Procedure, out var procValue))
+                    {
+                        CurrentProcedure = new KeyValuePair<string, string>(PatientCase.Procedure, procValue);
+                    }
+
+                    if (CodeDefinition.Codes.TryGetValue("VESS", out var vessDict) && vessDict.TryGetValue(PatientCase.Vessel, out var vessValue))
+                    {
+                        CurrentVessel = new KeyValuePair<string, string>(PatientCase.Vessel, vessValue);
+                    }
+
+                    if (CodeDefinition.Codes.TryGetValue("LOCT", out var loctDict) && loctDict.TryGetValue(PatientCase.Location, out var loctValue))
+                    {
+                        CurrentLocation = new KeyValuePair<string, string>(PatientCase.Location, loctValue);
+                    }
                 }
+
                 else
                 {
                     Dictionary<string, Object> sqlParameters = new Dictionary<string, Object>();
@@ -226,19 +236,19 @@ namespace RaywattApp.ViewModels
             parameter["patientCase"] = PatientCase;
             parameter["prevStatus"] = PrevStatus;
 
+            PatientCase.FlushMedia = SelectedFlushMedia;
+            PatientCase.PullbackType = SelectedPullbackType;
+            PatientCase.PullbackLength = PbLength + "0";
+            PatientCase.Procedure = CurrentProcedure.Key;
+            PatientCase.Vessel = CurrentVessel.Key;
+            PatientCase.Location = CurrentLocation.Key;
+
             if (DeviceStatus.CatheterStatus == Constants.CatheterStatusFailed)
             {
                 WeakReferenceMessenger.Default.Send(new NavigationMessage(Constants.RecordingCatheterFailPage) { Parameter = parameter });
             }
             else
             {
-                PatientCase.FlushMedia = SelectedFlushMedia;
-                PatientCase.PullbackType = SelectedPullbackType;
-                PatientCase.PullbackLength = PbLength + "0";
-                PatientCase.Procedure = CurrentProcedure.Key;
-                PatientCase.Vessel = CurrentVessel.Key;
-                PatientCase.Location = CurrentLocation.Key;
-
                 double sheathType = 2.6;
                 if (PatientCase.AccessionNumber.Equals("1.7"))
                     sheathType = 1.7;
