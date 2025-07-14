b36ef308a79f6ce300465bb4a6860bc165c48843
raywattTS <110812182+raywattTS@users.noreply.github.com>
2025-07-14 17:40:59 +0900

    Login 화면 종료 버튼 추가

--- RaywattApp/RaywattApp/ViewModels/OutsetLoginViewModel.cs
+++ RaywattApp/RaywattApp/ViewModels/OutsetLoginViewModel.cs
@@ -1,13 +1,17 @@
-﻿using System.Windows.Input;
+﻿using System.Collections.Generic;
+using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using log4net;
+using RaywattApp.Common.Angio;
 using RaywattApp.Common.Bases;
 using RaywattApp.Common.Dialog;
 using RaywattApp.Common.Messages;
+using RaywattApp.Common.Util;
 using RaywattApp.Models;
 using RaywattApp.Services;
+using RaywattApp.Views.Dialog;
 
 namespace RaywattApp.ViewModels
 {
@@ -29,6 +33,12 @@ namespace RaywattApp.ViewModels
         public ICommand LoginCommand
         {
             get { return this._loginCommand ?? (this._loginCommand = new RelayCommand(Login)); }
+        }
+
+        private ICommand _exitCommand;
+        public ICommand ExitCommand
+        {
+            get { return this._exitCommand ?? (this._exitCommand = new RelayCommand(Exit)); }
         }
 
         public OutsetLoginViewModel(SqlManager sqlManager, IDialogService dialogService)
@@ -59,6 +69,21 @@ namespace RaywattApp.ViewModels
                 WeakReferenceMessenger.Default.Send(new NavigationMessage(Constants.UserListPage));
             else
                 WeakReferenceMessenger.Default.Send(new NavigationMessage(Constants.OutsetLoadingPage));
+        }
+
+        private void Exit()
+        {
+            _log.Debug("Exit");
+
+            Dictionary<string, object> parameter = new Dictionary<string, object>();
+            parameter["title"] = _l10n["Power Off"];
+            parameter["message"] = _l10n["Are you sure you want to shut down the power?"];
+            var result = _dialogService.OpenDialog(new ConfirmDialogControl(), parameter, Constants.ApplicationWidth, Constants.ApplicationHeight);
+
+            if (result != null && result.DialogAnswer == DialogResults.Answer.Yes)
+            {
+                CommonUtil.Exit(DeviceStatus, null, true, true);
+            }
         }
     }
 }
