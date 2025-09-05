64c2995be58f7d8bf6b0873248ee3be5014dabc9
KimTwin <twin4184@gmail.com>
2025-09-05 21:10:58 +0900

    1차 에러 해결
    1차 과정에서 쓰레기값이 입력되는 오류로 인해, 심하게 어긋나는 문제를 제거하였습니다.
    2차 과정은 추가 수정이 필요한 상태입니다.

--- RayCore/RayCore/OCTSystem.cpp
+++ RayCore/RayCore/OCTSystem.cpp
@@ -1701,6 +1701,8 @@ UINT COCTSystem::threadAutoCalibration(LPVOID param) {
 		// way1
 		int minVal = INT_MAX, maxVal = 0, Loc = startPosition;
 		for (int i = 0; i < pSystem->m_vCalibrationInfo.size(); i++) {
+			if(pSystem->m_vCalibrationInfo.at(i).first <= 10)
+				continue;
 			if (pSystem->m_vCalibrationInfo.at(i).first < minVal) {
 				minVal = pSystem->m_vCalibrationInfo.at(i).first;
 				Loc = pSystem->m_vCalibrationInfo.at(i).second;
