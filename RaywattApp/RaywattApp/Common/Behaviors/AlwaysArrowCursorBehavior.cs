7cc040853d50f5a9bd98e398896c67169b95d30f
hwjungRayWatt <hwjung@raywatt.com>
2025-09-15 19:17:38 +0900

    [FIX] 달력 수행 시, 커서 풀리는 현상 수정 Behavior를 통하여 커서 설정

---
+++ RaywattApp/RaywattApp/Common/Behaviors/AlwaysArrowCursorBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace RaywattApp.Common.Behaviors
+{
+    public static class AlwaysArrowCursorBehavior
+    {
+        public static readonly DependencyProperty EnabledProperty =
+            DependencyProperty.RegisterAttached(
+                "Enabled",
+                typeof(bool),
+                typeof(AlwaysArrowCursorBehavior),
+                new PropertyMetadata(false, OnEnabledChanged));
+
+        public static void SetEnabled(DependencyObject obj, bool value) => obj.SetValue(EnabledProperty, value);
+        public static bool GetEnabled(DependencyObject obj) => (bool)obj.GetValue(EnabledProperty);
+
+        private static void OnEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is UIElement el)
+            {
+                if ((bool)e.NewValue)
+                {
+                    if (Application.Current.TryFindResource("arrow") is Cursor customCursor)
+                        Mouse.OverrideCursor = customCursor;
+                }
+                else
+                {
+                    Mouse.OverrideCursor = null;
+                }
+            }
+        }
+    }
+}
