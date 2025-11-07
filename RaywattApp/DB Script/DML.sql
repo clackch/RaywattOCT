b50e8af3440d82867bfc9b7bd8f33a1ba596830b
raywattTS <110812182+raywattTS@users.noreply.github.com>
2025-11-07 15:48:23 +0900

    Admin 계정에서 기관 정보 저장 필드 추가

--- RaywattApp/DB Script/DML.sql
+++ RaywattApp/DB Script/DML.sql
@@ -12,6 +12,7 @@ INSERT INTO rv_schema.configuration(classification, key, value, buffer) VALUES (
 INSERT INTO rv_schema.configuration(classification, key, value, buffer) VALUES ('Password', 'ExpiryDay', '90', '');
 INSERT INTO rv_schema.configuration(classification, key, value, buffer) VALUES ('Password', 'MaxCount', '5', '');
 INSERT INTO rv_schema.configuration(classification, key, value, buffer) VALUES ('Password', 'WaitSecond', '30', '');
+INSERT INTO rv_schema.configuration(classification, key, value, buffer) VALUES ('Institute', 'Info', '', '');
 INSERT INTO rv_schema.configuration(classification, key, value, buffer) VALUES ('AutoPB', 'LumenMin', '2', '');
 INSERT INTO rv_schema.configuration(classification, key, value, buffer) VALUES ('AutoPB', 'LumenMax', '70', '');
 INSERT INTO rv_schema.configuration(classification, key, value, buffer) VALUES ('AutoPB', 'SNR', '1', '');
