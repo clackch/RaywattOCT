63f54bcb8de02e357fa396b12de5286b8421aa13
hwjungRayWatt <hwjung@raywatt.com>
2025-11-10 16:39:05 +0900

    [FIX] RV200, RV201 분기 추가 - RV200: configuration의 Terms&Cond의 buffer의 값으로 dicom 저장 - RV201: configuration의 Institute의 value 값으로 dicom 저장 - Admin의 UserListPage에 언더바 표시 되도록 수정

--- RaywattApp/RaywattApp/ViewModels/File/FileExportStep2DicomViewModel.cs
+++ RaywattApp/RaywattApp/ViewModels/File/FileExportStep2DicomViewModel.cs
@@ -118,7 +118,7 @@ namespace RaywattApp.ViewModels.File
                     ExportSize += frameSize * patientCase.NumOfFrames;
                 }
             }
-            else if(FileExport.Material == Constants.ExportMaterialBookmarked)
+            else if (FileExport.Material == Constants.ExportMaterialBookmarked)
             {
                 ExportSize = frameSize * FileExport.BookmarkedFrames.Count;
             }
@@ -143,7 +143,7 @@ namespace RaywattApp.ViewModels.File
                     patientCase.Id = alternateId + "_" + patientCase.Id.Split("_")[1];
                     patientCase.PatientId = alternateId;
                     patientCase.PatientName = Constants.ExportAnonymous;
-                    patientCase.Birthdate = new DateTime(1900, 1, 1);                    
+                    patientCase.Birthdate = new DateTime(1900, 1, 1);
                 }
             }
 
@@ -159,7 +159,7 @@ namespace RaywattApp.ViewModels.File
                 parameter["selectedDicomServer"] = SelectedDicomServer;
                 parameter["usePeerVerification"] = CommonUtil.IsTestMode(DeviceStatus.TestMode, "CertIgnore") == true ? false : true;
             }
-                
+
             var result = _dialogService.OpenDialog(new FileCopyDialogControl(), parameter, Constants.FileExportDialogWidth, Constants.FileExportDialogHeight);
 
             RayExportWrapper.DestroyDcmClient(dicomClient);
@@ -174,31 +174,36 @@ namespace RaywattApp.ViewModels.File
 
             Dictionary<string, string> dicomProperty = new Dictionary<string, string>();
 
-            foreach(StringModel temp in dicomPropertyList)
+            foreach (StringModel temp in dicomPropertyList)
             {
                 dicomProperty.Add(temp.ReturnString, temp.ReturnString2);
             }
 
-            // Terms&Cond
-            var termsParams = new Dictionary<string, object>
+            if (CommonUtil.IsRV200())
             {
-                ["classification"] = "Terms&Cond"
-            };
-            IList<Configuration> tnCs = _sqlManager.SelectConfiguration(termsParams);
-            if (tnCs != null && tnCs.Count > 0)
-            {
-                dicomProperty["00080080"] = tnCs[0].Buffer;
+                // Terms&Cond
+                var termsParams = new Dictionary<string, object>
+                {
+                    ["classification"] = "Terms&Cond"
+                };
+                IList<Configuration> tnCs = _sqlManager.SelectConfiguration(termsParams);
+                if (tnCs != null && tnCs.Count > 0)
+                {
+                    dicomProperty["00080080"] = tnCs[0].Buffer;
+                }
             }
-
-            // Institute
-            var instituteParams = new Dictionary<string, object>
+            else
             {
-                ["classification"] = "Institute"
-            };
-            IList<Configuration> institute = _sqlManager.SelectConfiguration(instituteParams);
-            if (institute != null && institute.Count > 0)
-            {
-                dicomProperty["00080070"] = institute[0].Value;
+                // Institute
+                var instituteParams = new Dictionary<string, object>
+                {
+                    ["classification"] = "Institute"
+                };
+                IList<Configuration> institute = _sqlManager.SelectConfiguration(instituteParams);
+                if (institute != null && institute.Count > 0)
+                {
+                    dicomProperty["00080070"] = institute[0].Value;
+                }
             }
 
             return dicomProperty;
@@ -216,7 +221,7 @@ namespace RaywattApp.ViewModels.File
                     parameter["message"] = _l10n["Select PACS server"];
                     var result = _dialogService.OpenDialog(new AlertDialogControl(), parameter, Constants.ApplicationWidth, Constants.ApplicationHeight);
                     return;
-                }                    
+                }
 
                 //Connection Test
                 bool res = await ConnectionTest();
@@ -272,7 +277,7 @@ namespace RaywattApp.ViewModels.File
                 Dictionary<string, Object> data = (Dictionary<string, Object>)result.DialogReturn;
                 DicomServer dicomServer = (DicomServer)data["selectedDicomServer"];
                 SelectedDicomServer = dicomServer;
-                
+
             }
         }
     }
